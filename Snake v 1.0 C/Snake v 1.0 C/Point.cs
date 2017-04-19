9bcf04934cc96288aadcffece73b312a6bcfff3c
TauTen <S_zakrepa@bigmir.net>
2017-04-19 15:26:09 +0300

    3

---
+++ Snake v 1.0 C/Snake v 1.0 C/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_v_1._0_C
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
